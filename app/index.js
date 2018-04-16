cff0d68e560f527ba7acb59aecb807cfba9a6f7c
Jean Paul Gomez <jeanpaul.gomez@gmail.com>
2018-04-15 23:41:13 -0400

    Added Navigation

---
+++ app/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { View, AsyncStorage } from 'react-native';
+import { Router, Scene } from 'react-native-router-flux';
+import Home from './component/home';
+
