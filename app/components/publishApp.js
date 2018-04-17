e21261498435619ce91bbd782e9e128365903544
kaw28 <kaw28@njit.edu>
2018-04-16 23:36:45 -0400

    Top Navigation in place, with static content

---
+++ app/components/publishApp.js
@@ -0,0 +1,19 @@
+import React, { Component, View } from 'react';
+
+import Home from './home';
+import TopNavigation from './topNavigation';
+
+
+
+
+
+const App = () => (
+    <div>
+        <TopNavigation />
+        <Home />
+    </div>
+)
+
+export default App;
+
+
---
+++ app/components/topNavigation.js
@@ -0,0 +1,43 @@
+import React ,{Component} from 'react';
+import {
+    StyleSheet,
+    FlatList,
+    View,
+    Text,
+    Image,
+    ActivityIndicator
+} from 'react-native';
+
+class Navigation extends Component {
+    render() {
+
+    }
+}
+
+
+
+
+const styles = StyleSheet.create({
+
+
+    row:{
+        borderBottomWidth: 1,
+        borderColor: "#ccc",
+        padding: 10
+    },
+
+    title:{
+        fontSize: 20,
+        fontWeight: "600"
+    },
+
+    description:{
+        marginTop: 5,
+        fontSize: 14,
+    },
+    poster:{
+        width: 150,
+        height: 250,
+        alignItems: 'center'
+    }
+});
