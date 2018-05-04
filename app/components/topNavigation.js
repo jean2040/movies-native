0e9dbc61a7589e473ce96825618956ad08c2c5f3
Admin <kaw28@njit.edu>
2018-05-04 14:14:50 -0400

    Toggle switch changing title bar, next will work on changing data in flat list.

--- app/components/topNavigation.js
+++ app/components/topNavigation.js
@@ -9,13 +9,52 @@ import { Ionicons } from '@expo/vector-icons';
 
 
 
+
 export default class TopNavigation extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        switchValue: true,
+        titleText: 'Movies',
+    };
+    _handleToggleSwitch = () => this.setState(state => ({
+        switchValue: !state.switchValue,
+        titleText: changeText(state.switchValue),
+    }));
+
+
+
+
+
     render() {
         return (
             <View style={{flex: 1, flexDirection: 'row'}}>
-                <Switch onSyncPress={value => this.setState({value})}/>
-
+                <Switch
+                    onValueChange={this._handleToggleSwitch}
+                    value = {this.state.switchValue}
+                />
+                <Text style={styles.titleType}>
+                    {this.state.titleText}
+                </Text>
             </View>
         );
     }
 }
+
+function changeText(current){
+    if(current){
+        return "TV Shows"
+    }else {
+        return "Movies"
+    }
+}
+
+const styles = StyleSheet.create({
+    titleType: {
+        fontWeight: 'bold',
+        fontSize: 25,
+        marginTop: 6,
+    },
+});
